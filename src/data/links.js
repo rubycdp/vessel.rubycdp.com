253a27ab5bc0ab3f017f90abc8a2aefddb8b10b6
Nikita Makarin <zn.makarin@gmail.com>
2020-04-21 11:20:15 +0300

    Fork ferrum-website project

---
+++ src/data/links.js
@@ -0,0 +1,6 @@
+export default {
+  ferrum: 'https://github.com/route/ferrum',
+  cuprite: 'https://github.com/machinio/cuprite',
+  vessel: 'https://github.com/route/vessel',
+  evrone: 'https://evrone.com/ruby'
+}
